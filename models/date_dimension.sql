9a62efa4fe844b55c36a9c2117b0fde898566966
Vadym Polianskyi <vadym.polianskyi@dataart.com>
2025-11-20 14:12:45 +0000

    Implement weather dimensions

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -5,15 +5,10 @@ with
             date(to_timestamp(started_at)) as date_started_at,
             hour(to_timestamp(started_at)) as hour_started_at,
             dayname(to_timestamp(started_at)) as day_name_started_at,
-            case
-                when dayname(to_timestamp(started_at)) in ('Sat', 'Sun')
-                then 'weekend'
-                else 'businessday'
-            end as day_type,
-            month(to_timestamp(started_at)) as month_started_at
+            {{ get_day_type("started_at") }} as day_type,
+            {{ station_of_year("started_at") }} as station_of_year
         from {{ source("demo", "bike") }}
         where started_at != 'started_at'
     )
-
 select *
 from cte
